230ef23a50b744baeee8cd867814078144ed0377
devvspaces <netrobepy@gmail.com>
2024-02-08 09:02:04 +0100

    Complete project

--- src/form/views.py
+++ src/form/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import CreateView
 from django.http import JsonResponse
+from .models import FlightForm
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class FlightForm(CreateView):
+    model = FlightForm
+    fields = '__all__'
+
     def form_valid(self, form):
         return JsonResponse({
             'message': 'Your form is submitted successfully!'})
